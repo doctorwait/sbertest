23ba706ee1d55265b657cd2b2d49a27079dd31e6
doctorwait <prosnites@yandex.ru>
2022-11-19 13:16:34 +0400

    I made choise which site

--- test_general.py
+++ test_general.py
@@ -1,25 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 import time
 
-main_page = 'https://www.sberbank.com/index'
+main_page = 'http://the-internet.herokuapp.com/'
+
+
+@pytest.fixture(scope='function')
+def driver():
+    print("\nstart browser for test..")
+    driver = webdriver.Chrome()
+    yield driver
+    print("\nquit browser..")
+    driver.quit()
 
 
 class TestMainPage:
-    @pytest.fixture(scope='class')
-    def driver(self):
-        print("\nstart browser for test..")
-        driver = webdriver.Chrome()
-        yield driver
-        print("\nquit browser..")
-        driver.quit()
-
-    def test_main_page(self, driver):
+    def test_A_B(self, driver):
         driver.get(main_page)
-        time.sleep(5)
-        logo = driver.find_element(by='css selector', value='.header__logo-image') #.implicitly_wait(5)
-        print(logo.text)
+        driver.find_element(by='css selector', value='[href="/abtest"]').click()
 
+    @pytest.mark.skip
+    def test_add_remove_elements(self, driver):
+        driver.get(main_page)
+        driver.find_element(by='css selector', value='.kitt-header-sections__dropdown')
 
-pytest.main()
+    @pytest.mark.skip
+    def test_about_the_bank(self, driver):
+        driver.get(main_page)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "li.kitt-top-menu__item_first:nth-child(1) > a:nth-child(2)"))).click()
+
