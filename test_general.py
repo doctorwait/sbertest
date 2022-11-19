c818a668f03731bcb32eecfaeb3488060d3e3127
doctorwait <prosnites@yandex.ru>
2022-11-19 17:14:58 +0400

    Wrote 4 functions

--- test_general.py
+++ test_general.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import pytest
+import requests
+
 import time
 
 main_page = 'http://the-internet.herokuapp.com/'
@@ -18,18 +18,43 @@ def driver():
 
 
 class TestMainPage:
+    @pytest.mark.skip
     def test_A_B(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/abtest"]').click()
 
     @pytest.mark.skip
     def test_add_remove_elements(self, driver):
+        def list_of_elems():
+            return driver.find_elements(by='class name', value='added-manually')
+        how_much = 5
         driver.get(main_page)
-        driver.find_element(by='css selector', value='.kitt-header-sections__dropdown')
+        driver.find_element(by='css selector', value='[href="/add_remove_elements/"]').click()
+        adder = driver.find_element(by='css selector', value='.example>:nth-child(1)')
+        [adder.click() for _ in range(how_much)]
+        elems = list_of_elems()
+        assert len(elems) == how_much, "Invalid number of items created."
+        [el.click() for el in elems]
+        elems = list_of_elems()
+        assert len(elems) == 0, "Not all items have been removed."
 
     @pytest.mark.skip
-    def test_about_the_bank(self, driver):
+    def test_basic_auth(self, driver):
         driver.get(main_page)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "li.kitt-top-menu__item_first:nth-child(1) > a:nth-child(2)"))).click()
+        driver.find_element(by='css selector', value='[href="/basic_auth"]').click()
+        # There are no hooks in the HTML code that will allow to get to the active window. Also, when logging in,
+        # the window does not close. In this case, you need to contact the front-end developers (who don't exist).
+        driver.switch_to.active_element.send_keys('user', Keys.TAB, 'admin', Keys.TAB, Keys.TAB, Keys.ENTER)
+
+    @pytest.mark.skip
+    def test_broken_images(self, driver):
+        driver.get(main_page)
+        driver.find_element(by='css selector', value='[href="/broken_images"]').click()
+        images = driver.find_elements(by='css selector', value='#content>div>img')
+        for image in images:
+            link = image.get_attribute('src')
+            assert requests.get(link).status_code == 200, 'The web page with the image is not available.'
+
+    def challenging_dom(self, driver):
+        driver.get(main_page)
 
