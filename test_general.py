d50721d5fcf58c1d5b992f1d9a965d5d327c153b
doctorwait <prosnites@yandex.ru>
2022-11-20 16:55:28 +0400

    One more test function

--- test_general.py
+++ test_general.py
@@ -6,8 +6,11 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
 import pytest
 import requests
+import pyautogui
+import time
 
 
 main_page = 'http://the-internet.herokuapp.com/'
@@ -23,12 +26,12 @@ def driver():
 
 
 class TestMainPage:
-
+    @pytest.mark.skip
     def test_A_B(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/abtest"]').click()
 
-
+    @pytest.mark.skip
     def test_add_remove_elements(self, driver):
         def list_of_elems():
             return driver.find_elements(by='class name', value='added-manually')
@@ -43,7 +46,7 @@ class TestMainPage:
         elems = list_of_elems()
         assert len(elems) == 0, "Not all items have been removed."
 
-
+    @pytest.mark.skip
     def test_basic_auth(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/basic_auth"]').click()
@@ -51,7 +54,7 @@ class TestMainPage:
         # the window does not close. In this case, you need to contact the front-end developers (who don't exist).
         driver.switch_to.active_element.send_keys('user', Keys.TAB, 'admin', Keys.TAB, Keys.TAB, Keys.ENTER)
 
-
+    @pytest.mark.skip
     def test_broken_images(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/broken_images"]').click()
@@ -60,7 +63,7 @@ class TestMainPage:
             link = image.get_attribute('src')
             assert requests.get(link).status_code == 200, 'The web page with the image is not available.'
 
-
+    @pytest.mark.skip
     def test_challenging_dom(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/challenging_dom"]').click()
@@ -68,14 +71,14 @@ class TestMainPage:
         driver.find_element(by='css selector', value='.large-2>:nth-child(3)').click()
         driver.find_element(by='css selector', value='.large-2>:nth-child(5)').click()
 
-
+    @pytest.mark.skip
     def test_checkboxes(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/checkboxes"]').click()
         driver.find_element(by='css selector', value='#checkboxes>:nth-child(1)').click()
         driver.find_element(by='css selector', value='#checkboxes>:nth-child(3)').click()
 
-
+    @pytest.mark.skip
     def test_context_menu(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/context_menu"]').click()
@@ -86,7 +89,7 @@ class TestMainPage:
         alert = driver.switch_to.alert
         assert alert.text == 'You selected a context menu', "Right-clicking on a selection didn't work."
 
-
+    @pytest.mark.skip
     def test_dissapearing_button(self, driver):
         def try_to_find_an_element():
             return driver.find_element(by='css selector', value='[href="/gallery/"]')
@@ -97,7 +100,7 @@ class TestMainPage:
             driver.back()
             driver.refresh()
 
-
+    @pytest.mark.skip
     def test_drag_and_drop(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/drag_and_drop"]').click()
@@ -111,14 +114,14 @@ class TestMainPage:
         driver.execute_script(script + "$('#column-a').simulateDragDrop({ dropTarget: '#column-b'});")
         assert driver.find_element(by='css selector', value='#column-a>header').text == 'B', "Blocks are not swapped."
 
-
+    @pytest.mark.skip
     def test_dropdown(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/dropdown"]').click()
         lst = Select(driver.find_element(by='id', value='dropdown'))
         lst.select_by_value('1')
 
-
+    @pytest.mark.skip
     def test_dynamic_content(self, driver):
         def rows():
             return driver.find_elements(by='css selector', value='div.large-10:nth-child(1) > .row > .large-10')
@@ -129,7 +132,7 @@ class TestMainPage:
         texts2 = [row.text for row in rows()]
         assert texts1 == texts2, 'Refreshing the page changes the content.'
 
-
+    @pytest.mark.skip
     def test_dynamic_controls(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href = "/dynamic_controls"]').click()
@@ -144,9 +147,19 @@ class TestMainPage:
         else:
             assert False, "The checkbox should have disappeared from the page."
 
-
+    @pytest.mark.skip
     def test_entry_ad(self, driver):
         driver.get(main_page)
         driver.find_element(by='css selector', value='[href="/entry_ad"]').click()
         driver.switch_to.active_element.send_keys(Keys.ENTER)
+
+    @pytest.mark.skip
+    def test_out_of_the_viewport(self, driver):
+        driver.get(main_page)
+        driver.find_element(by='css selector', value='[href="/exit_intent"]').click()
+        pyautogui.moveTo(1000, 1000, 0.2)
+        pyautogui.moveTo(0, 0, 0.2)
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.modal-footer > p')))
+        driver.switch_to.active_element.find_element(by='css selector', value='.modal-footer > p').click()
+
 
